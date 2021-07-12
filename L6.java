08fbfe693d348f4441b56385c549d46d44e5b5f8
tec-km <kamiyama2021@gmail.com>
2021-07-13 06:22:17 +0900

    数

--- L6.java
+++ L6.java
@@ -11,5 +11,9 @@ System.out.println(var);
 System.out.println(var2);
 
 
+// 変数は数字で始められない。記号は＄と_のみ。 
+// 大文字と小文字は区別される。
+// 予約後もだめ return class for whileなど
+
  }
 }
---
+++ L7.java
@@ -0,0 +1,32 @@
+class L7 {
+  public static void main(String[] args){
+   
+byte var01 = 1;
+short var02 = 12345;
+int var03 = 1234567890;
+long var04 = 123456789000l;
+
+// byte -100~100
+// short -3万〜3万
+// int -20億〜20億
+// long 京の桁以上 最後にLをつける。
+
+
+System.out.println(var01);
+System.out.println(var02);
+System.out.println(var03);
+System.out.println(var04);
+
+
+float var05 = 1.234F;
+double var06 = 1.123456789;
+
+System.out.println(var05);
+System.out.println(var06);
+
+// floatは最後にF doubleの方が多くの桁を扱える。こちらを使うことが多い。
+
+}
+
+
+}

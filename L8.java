658c88d249f64aab7f8714eccf50bc866917e3ce
tec-km <kamiyama2021@gmail.com>
2021-07-16 18:45:39 +0900

    配列

---
+++ L8.java
@@ -0,0 +1,41 @@
+class  L8 {
+  public static void main(String[] args){
+    String[] arr;
+
+    arr = new String[3];
+    arr[0]="sato";
+    arr[1]="suzuki";
+    arr[2]="takahashi";
+
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+
+
+// 配列の作り方。宣言と代入を別にやるか同時にやるか。
+
+// 別の場合
+// データ型[]配列変数名;
+// 配列変数名=new データ型[要素数];
+
+
+
+  String[] arr2 = {"sato","suzuki","takahashi"};
+
+  System.out.println(arr2[0]);
+  System.out.println(arr2[1]);
+  System.out.println(arr2[2]);
+
+// 同時の場合
+// データ型[]配列変数={"データ１","データ２","データ３"};
+
+
+
+}
+
+
+
+
+
+}

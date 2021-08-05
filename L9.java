28ae1e5c77fa15ab77f06f314dace8bdcc88613b
tec-km <kamiyama2021@gmail.com>
2021-08-05 17:31:45 +0900

    算術演算子

--- L9.java
+++ L9.java
@@ -1,3 +1,14 @@
 class L9 {
-  
+    public static void main(String[] args) {
+      int x = 10;
+      int y = 2;
+
+      System.out.println(x+y);
+      System.out.println(x-y);
+      System.out.println(x*y);
+      System.out.println(x/y);
+      System.out.println(x%y);
+
+  } 
 }
+// 算術演算子 足し算 + 引き算 - 掛け算* 割り算 / 剰余 %

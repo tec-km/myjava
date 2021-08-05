1a3a1bfa003c520812fef23b7ba3659b6d196fd7
tec-km <kamiyama2021@gmail.com>
2021-08-05 17:43:57 +0900

    以上以下

--- L9.java
+++ L9.java
@@ -18,10 +18,18 @@ class L9 {
       System.out.println(a > b);
       System.out.println(a < b);
  
-
-
-
       
+      int c = 10;
+      int d = 2;
+      int e = 10;
+
+      System.out.println(c >= d);
+      System.out.println(c <= d);
+      System.out.println(c <= e);
+
+
+
+
     }
 
 }

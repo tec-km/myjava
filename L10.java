375b21ec0cfa132311676f5f07d219bfd7a5f72f
tec-km <kamiyama2021@gmail.com>
2021-08-05 19:24:37 +0900

    if文

---
+++ L10.java
@@ -0,0 +1,18 @@
+class L10 {
+  public static void main(String[] args) {
+
+// 条件分岐 if文   
+// if(条件){
+//     処理;
+// }
+    int age = 22;
+
+    if (age >= 20){
+        System.out.println("adult");
+
+
+    }
+
+  }
+
+}
--- L9.java
+++ L9.java
@@ -71,8 +71,6 @@ class L9 {
       System.out.println(o);
       System.out.println(p);
 
-
-
     }
 
 }

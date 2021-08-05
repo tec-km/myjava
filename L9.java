6419eb09c522d709a6f12eed992f1b11b4e38324
tec-km <kamiyama2021@gmail.com>
2021-08-05 17:38:58 +0900

    関係演算子

--- L9.java
+++ L9.java
@@ -1,14 +1,27 @@
 class L9 {
     public static void main(String[] args) {
+
       int x = 10;
       int y = 2;
 
+// 算術演算子 足し算 + 引き算 - 掛け算* 割り算 / 剰余 %
       System.out.println(x+y);
       System.out.println(x-y);
       System.out.println(x*y);
       System.out.println(x/y);
       System.out.println(x%y);
 
-  } 
+// 関係演算子 正しい場合はTrue
+      int a = 10;
+      int b = 2;
+ 
+      System.out.println(a > b);
+      System.out.println(a < b);
+ 
+
+
+
+      
+    }
+
 }
-// 算術演算子 足し算 + 引き算 - 掛け算* 割り算 / 剰余 %

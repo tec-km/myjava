14f98c7a92b32a24338c47004553e71f62f3fdab
tec-km <kamiyama2021@gmail.com>
2021-08-07 03:34:19 +0900

    for文

---
+++ L11.java
@@ -0,0 +1,20 @@
+class L11 {
+  public static void main(String[] args) {
+
+// 繰り返し 反復処理 for文 条件を満たしてれば繰り返す。
+// for（スタート値;継続条件;増減式）{    
+//  繰り返し処理
+//  }
+
+   for(int i = 0;i <= 4; i ++){
+     System.out.println(i);
+   }
+
+
+
+
+
+
+
+  }
+}

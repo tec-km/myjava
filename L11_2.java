60f72b2f39e8e3b00e99894cddd44e61181e024b
tec-km <kamiyama2021@gmail.com>
2021-08-12 00:01:22 +0900

    配列２

--- L11_2.java
+++ L11_2.java
@@ -1,9 +1,12 @@
 class L11_2 {
   public static void main(String [] args){
 int arr[] = {2,4,6,8,10};
+int sum = 0;
+
 
 for( int i = 0; i <= 4; i++ ){
-  System.out.println(arr[i]);
+  sum += arr[i];
 }
+  System.out.println(sum);
   }
 }

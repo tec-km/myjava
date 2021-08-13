85c88edc36137c70014f2230be679ab0c22ca33e
tec-km <kamiyama2021@gmail.com>
2021-08-13 18:55:14 +0900

    メソッド

--- L12.java
+++ L12.java
@@ -1,5 +1,23 @@
-class L12 {
-  public static void main(String[] args) {
+class L12 {   //←クラスの定義            
+  public static void main(String[] args) {    //←メソッドの定義
+    // メソッドとは処理のこと。同じものを使える。他のところです使える。
+    // printlnメソッド一行で何行ものコードが入っている。
+
+// アクセス修飾子   
+// public 他のクラスからアクセスが可能
+// protected 同じパッケージサブクラス
+// 指定無し 同じパッケージ
+// private 同一クラスからアクセス可能
+
+
+
+
+
+
+
+
+
+
 }
 }
 
---
+++ L13.java
@@ -0,0 +1,9 @@
+import jdk.internal.foreign.abi.Binding.VMLoad;
+
+class L13 {
+  String name = "sato";
+ public void calculateAvg(int math, int english){
+ System.out.println((math + english)/2);
+ }
+
+}

d58e65cdac4d01c543b80d004590d6757bc4282e
tec-km <kamiyama2021@gmail.com>
2021-08-28 09:25:34 +0900

    pg

--- progate1.java
+++ progate1.java
@@ -33,8 +33,24 @@ class progate1 {
     
     // 38と19を足して出力してください
     System.out.println ( 38 + 19);
+      
+    // int型の変数numberを定義してください
+    int number;
+      
+    // 変数numberに3を代入してください
+    number = 3;
     
+    // 変数numberを出力してください
+    System.out.println(number);
 
+    // String型の変数nameを定義してください
+    String name;
+    
+    // 変数nameに"Wanko"を代入してください
+    name = "Wanko";
+    
+    // 変数nameを出力してください
+    System.out.println(name);
 
 
 

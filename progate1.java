764a5aca0acec11d8422b5ef7bc95bdae8fe9454
tec-km <kamiyama2021@gmail.com>
2021-08-28 09:06:55 +0900

    プロゲート

--- progate1.java
+++ progate1.java
@@ -1,9 +1,11 @@
-class Main {
+class progate1 {
   public static void main(String[] args) {
     System.out.println("Hello Java");
   }
 }
 
+// コメントアウト
 // javac progate1.java
 // コンパイルする
 // java progate1
+

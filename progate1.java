b83b6077c9ba7a968a07cf344e6bee94476a92f8
tec-km <kamiyama2021@gmail.com>
2021-08-29 23:58:43 +0900

    pg

--- progate1.java
+++ progate1.java
@@ -53,6 +53,22 @@ class progate1 {
     System.out.println(name);
 
 
+    int number1 = 3;
+    
+    // int型の変数number2を定義し、7を代入してください
+    int number2 = 7;
+    
+    // number1 * number2を出力してください
+   System.out.println(number1 * number2); 
+    
+    // 変数textに「プログラミングを勉強しよう」を代入してください
+    String text = "プログラミングを勉強しよう";
+    
+    // 「Progateで」と変数textを連結して出力してください
+    System.out.println("Progateで" + text);
+    
+
+    
 
 
 

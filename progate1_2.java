01f8c0bbfb02a70914cb2a242b868f07842e384a
tec-km <kamiyama2021@gmail.com>
2021-08-30 23:26:35 +0900

    自己代入

--- progate1_2.java
+++ progate1_2.java
@@ -17,10 +17,32 @@ class Main {
     // 変数textを出力してください
     System.out.println(text);
     
-
-
-
-
+    int number2 = 3;
+    System.out.println(number2);
     
+    // 変数numberの値に7を足して、変数numberを上書きしてください
+     number2 =  number2 + 7;
+    
+    // 変数numberを出力してください
+    System.out.println(number2);
+      
+//////////////
+    int number3 = 8;
+    
+    // 変数numberに7をかけて、変数numberを上書きしてください
+    number3 *= 7;
+    
+    // 変数numberを出力してください
+    System.out.println(number3);
+    
+    // 変数numberの値に1を足して、変数numberを上書きしてください
+    number3 ++;  
+    
+    // 変数numberを出力してください
+    System.out.println(number3);
+    
+
+
+
   }
 }

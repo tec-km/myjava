e1f26b3cb7936991e80fa88c1dcd390c4b6bee91
tec-km <kamiyama2021@gmail.com>
2021-08-31 00:59:57 +0900

    double

--- progate1_2.java
+++ progate1_2.java
@@ -57,7 +57,19 @@ class Main {
     // 変数triangleAreaを出力してください
     System.out.println(triangleArea);
 
+//////// double型は小数点以下
+   // double型の変数number1を定義し、8.5を代入してください。
+    double number5 = 8.5;
 
+    // double型の変数number2を定義し、3.4を代入してください。
+    double number6 = 3.4;
+    
+    // number1にnumber2を足した値を出力してください
+    System.out.println( number5 + number6);
+    
+    // number1からnumber2を引いた値を出力してください
+    System.out.println (number5 - number6);
+    
 
 
   }

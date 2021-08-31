09eec5531526d3974d53b9ef771e771f10d6ae4d
tec-km <kamiyama2021@gmail.com>
2021-08-31 14:14:16 +0900

    Create progate2.java

---
+++ progate2.java
@@ -0,0 +1,48 @@
+public class progate2 {
+  
+  public static void main(String[] args) {
+    // 「true」を用いて、「真」を表す真偽値を出力してください
+    System.out.println(true);
+    
+    // 「false」を用いて、「偽」を表す真偽値を出力してください
+    System.out.println(false);
+    
+    // 「==」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 == 3);
+    
+    // 「!=」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 != 3);
+    
+    // 変数を定義し、値を比較した結果を代入してください
+    boolean bool = 3 * 9 == 27;
+    
+    // 変数boolの値を出力してください
+    System.out.println(bool);
+    
+
+/////////
+ // 8と5を比較し、falseとなるようにしてください
+ System.out.println( 8 < 5 );
+    
+ // 3と2を比較し、trueとなるようにしてください
+ System.out.println( 3 >= 2 );
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}

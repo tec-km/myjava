454f3a6afee58f19857f62cf1c80ead191c7051d
tec-km <kamiyama2021@gmail.com>
2021-08-31 14:14:47 +0900

    Update progate2.java

--- progate2.java
+++ progate2.java
@@ -28,7 +28,22 @@ public class progate2 {
  System.out.println( 3 >= 2 );
  
 
-
+///////
+   // trueと出力されるようにしてください
+   System.out.println(true || false);
+    
+   // falseと出力されるようにしてください
+   System.out.println(false && true);
+   
+   // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+   System.out.println(8 < 5 && 3 >= 2);
+   
+   // 「8 < 5」または「3 >= 2」の結果を出力してください
+   System.out.println(8 < 5 || 3 >= 2);
+   
+   // 「8 < 5」でない、の結果を出力してください
+   System.out.println(!(8 < 5));
+   
 
 
 

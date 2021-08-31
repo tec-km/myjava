ace3fab55d1186c96f8a80b3e1b8521a1b5607f9
tec-km <kamiyama2021@gmail.com>
2021-08-31 14:24:24 +0900

    Update progate2.java

--- progate2.java
+++ progate2.java
@@ -44,7 +44,29 @@ public class progate2 {
    // 「8 < 5」でない、の結果を出力してください
    System.out.println(!(8 < 5));
    
+/////
 
+   // 条件式に直接trueをいれてください
+   if (true) {
+    System.out.println("条件式がtrueのため、出力されます");
+  }
+  
+  // 条件式に直接falseをいれてください
+  if (false) {
+    System.out.println("条件式がfalseのため、出力されません");
+  }
+  
+  int x = 5;
+  // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
+  if (x > 2) { 
+    System.out.println("xは2より大きい");
+  }
+  
+  // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力してください
+  if (x > 8) { 
+    System.out.println("xは8より大きい");
+  }
+  
 
 
 

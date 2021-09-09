fae1402708f5371a9c640b7edcd21056b24b4874
tec-km <kamiyama2021@gmail.com>
2021-09-09 18:30:42 +0900

    Update progate2.java

--- progate2.java
+++ progate2.java
@@ -8,16 +8,16 @@ public class progate2 {
     System.out.println(false);
     
     // 「==」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 == 3);
+    //System.out.println(12 / 4 == 3);
     
     // 「!=」を用いて、値を比較した結果を出力してください
-    System.out.println(12 / 4 != 3);
+    //System.out.println(12 / 4 != 3);
     
     // 変数を定義し、値を比較した結果を代入してください
-    boolean bool = 3 * 9 == 27;
+    //boolean bool = 3 * 9 == 27;
     
     // 変数boolの値を出力してください
-    System.out.println(bool);
+    //System.out.println(bool);
     
 
 /////////
@@ -30,13 +30,13 @@ public class progate2 {
 
 ///////
    // trueと出力されるようにしてください
-   System.out.println(true || false);
+   //System.out.println(true || false);
     
    // falseと出力されるようにしてください
-   System.out.println(false && true);
+   //System.out.println(false && true);
    
    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
-   System.out.println(8 < 5 && 3 >= 2);
+   //System.out.println(8 < 5 && 3 >= 2);
    
    // 「8 < 5」または「3 >= 2」の結果を出力してください
    System.out.println(8 < 5 || 3 >= 2);
@@ -52,9 +52,9 @@ public class progate2 {
   }
   
   // 条件式に直接falseをいれてください
-  if (false) {
-    System.out.println("条件式がfalseのため、出力されません");
-  }
+  //if (false) {
+  // System.out.println("条件式がfalseのため、出力されません");
+  //}
   
   int x = 5;
   // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
